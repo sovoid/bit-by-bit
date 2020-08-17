d61ed1c60d83d0bc9d2e2bb271f3317eaaead3b8
und3fined-v01d <soham.parekh1998@gmail.com>
2020-08-17 20:58:05 +0530

    add: gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const alias = require('./webpack-aliases');
+const rssConfig = require('./rss-plugin-config');
+
+module.exports = {
+  siteMetadata: {
+    title: 'Bit by Bit | Soham Parekh',
+    author: 'Soham Parekh',
+    description: 'It took me a couple bytes',
+    siteUrl: 'https://bit-by-bit.netlify.app',
+    social: {
+      github: 'https://github.com/und3fined_v01d',
+      linkedin: 'https://linkedin.com/in/soham-parekh',
+      twitter: 'https://twitter.com/und3fined_v01d',
+    },
+  },
+  plugins: [
+    rssConfig,
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias,
+        extensions: [],
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-flow`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          post: require.resolve('./src/components/BlogPost/BlogPost.js'),
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/pages/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, 'src', 'assets', 'images'),
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID,
+        cookieExpires: 63072000, // two years
+      },
+    },
+  ],
+};
